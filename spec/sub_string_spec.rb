a78831a126d97ffb6563b7b85c32ed1c538b0b75
James <james.jcarline@gmail.com>
2022-11-24 23:15:15 +0000

    Set up spec test. Create placeholder method

--- spec/sub_string_spec.rb
+++ spec/sub_string_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 require_relative '../main/sub_string'
 
+dictionary = ['hello', 'hi', 'welcome', 'greetings', 'rest', 'rejoice', 'weary', 'celebrate']
+
 RSpec.describe 'Sub String' do
     
+    describe 'Substring exercise using set dictionary' do
+        it 'returns hash of two key-value pairs' do
+            expext(substring_compare('hello greetings', dictionary)).to eq("hello" => 1, "greetings" => 1)
+        end
+    end
 end
