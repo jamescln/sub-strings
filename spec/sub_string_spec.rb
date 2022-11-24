c920a31c8af4dbc85c0a178be142549bbdb01d62
James <james.jcarline@gmail.com>
2022-11-24 23:39:10 +0000

    Add second test. Correct typos

--- spec/sub_string_spec.rb
+++ spec/sub_string_spec.rb
@@ -6,8 +6,13 @@ dictionary = ['hello', 'hi', 'welcome', 'greetings', 'rest', 'rejoice', 'weary',
 RSpec.describe 'Sub String' do
     
     describe 'exercise using set dictionary' do
-        it 'will return hash of two key-value pairs' do
-            expext(substring_compare('hello greetings', dictionary)).to eq("hello" => 1, "greetings" => 1)
+
+        it 'will return a hash with one key-value pair when given a word' do
+            expect(substring_compare('hello', dictionary)).to eq("hello" => 1)
+        end
+
+        it 'will return hash of two key-value pairs when given two words' do
+            expect(substring_compare('hello greetings', dictionary)).to eq("hello" => 1, "greetings" => 1)
         end
     end
 end
