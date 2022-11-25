23d03c4c575fdfe1dd9f3776ba8d9a2549a518df
James <james.jcarline@gmail.com>
2022-11-25 12:36:42 +0000

    Convert arrays to hashes I have used the tally enumerable to convert the two arguments given to the substring_compare method from arrays to hashes.

--- main/sub_string.rb
+++ main/sub_string.rb
@@ -3,6 +3,11 @@ require 'pry-byebug'
 def substring_compare(content, dictionary)
     substring_array = content.split(" "); #convert content to an array
     # will likely use the to_h method on nested array
+    substring_tally = {}
+    dictionary_tally = {}
+    substring_tally = substring_array.tally(substring_tally)
+    dictionary_tally = dictionary.tally(dictionary_tally)
+    binding.pry
     common_words = substring_array.intersection(dictionary)
     binding.pry
 end
