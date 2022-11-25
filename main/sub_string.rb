1fce90f2569842afba13c99f356eaf6728a93e23
James <james.jcarline@gmail.com>
2022-11-25 07:56:19 +0000

    Add split method and pry debugger

--- main/sub_string.rb
+++ main/sub_string.rb
@@ -1,4 +1,10 @@
+require 'pry-byebug'
 
 def substring_compare(content, dictionary)
-    
+    substring_array = content.split(" ");
+    #substring_array.each do     
+    #end
+    binding.pry
 end
+
+test = substring_compare("hello test", ['hello', 'check']);
