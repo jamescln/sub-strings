a34ceb153d84237f2c3394e5350fc73c489d71ad
James <james.jcarline@gmail.com>
2022-11-25 14:57:33 +0000

    Convert methods in substring_compare to for loops
    The substring compare method couldn't deal with finding multiple values in the dictionary param.
    I've used nested for loops to iterate over the two arrays, compare the elements, and add them
    to a new array if they match. I'm then returning a tally of the new array.

--- main/sub_string.rb
+++ main/sub_string.rb
@@ -2,12 +2,21 @@ require 'pry-byebug'
 
 def substring_compare(content, dictionary)
     substring_array = content.split(" "); #convert content to an array
-    # will likely use the to_h method on nested array
-    substring_tally = {}
-    dictionary_tally = {}
-    substring_tally = substring_array.tally(substring_tally).to_a
-    dictionary_tally = dictionary.tally(dictionary_tally).to_a
-    return substring_tally.intersection(dictionary_tally).to_h
+    match_array = []
+    for i in 0..(substring_array.length-1) do
+        for j in 0..(dictionary.length-1) do
+            if substring_array[i] == dictionary[j]
+                match_array << dictionary[j]
+            end
+        end
+    end
+    match_tally = {}
+    return match_tally = match_array.tally(match_tally)
+    #substring_tally = {}
+    #dictionary_tally = {}
+    #substring_tally = substring_array.tally(substring_tally).to_a
+    #dictionary_tally = dictionary.tally(dictionary_tally).to_a
+    #return substring_tally.intersection(dictionary_tally).to_h
 end
 
 test = substring_compare("hello test", ['hello', 'check']);
