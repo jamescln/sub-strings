64da6892e5c4fd0f3b0af0b56c79059562f6fe8c
James <james.jcarline@gmail.com>
2022-11-25 15:57:24 +0000

    Clean unused code

--- main/sub_string.rb
+++ main/sub_string.rb
@@ -2,21 +2,16 @@ require 'pry-byebug'
 
 def substring_compare(content, dictionary)
     substring_array = content.split(" "); #convert content to an array
-    match_array = []
+    match_array = [] # create empty array to push elements to
     for i in 0..(substring_array.length-1) do
         for j in 0..(dictionary.length-1) do
             if substring_array[i] == dictionary[j]
-                match_array << dictionary[j]
+                match_array << dictionary[j] # push matching elements to array
             end
         end
     end
-    match_tally = {}
+    match_tally = {} # create empty hash to fill with the tally enumerable
     return match_tally = match_array.tally(match_tally)
-    #substring_tally = {}
-    #dictionary_tally = {}
-    #substring_tally = substring_array.tally(substring_tally).to_a
-    #dictionary_tally = dictionary.tally(dictionary_tally).to_a
-    #return substring_tally.intersection(dictionary_tally).to_h
 end
 
 test = substring_compare("hello test", ['hello', 'check']);
